438ea5cf725de582fc0304c929c7bba26c3ea1c1
Aljaetc <daasvsvami@gmail.com>
2020-06-23 19:09:12 +0300

    end lesson 2

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 let data = {
-	// title: 'Custom header for modal window',
+	title: 'Custom header for modal window',
 	closable: true,
-	content: 'Some custom content for modal window',
+	content: `
+		<p>Lorem ipsum dolor sit.</p>
+		<p>Lorem ipsum dolor sit.</p>
+	`,
 	width: '500px',
+	footerButtons: [
+		{
+			text: 'Ok',
+			type: 'primary',
+			handler() {
+				console.log('Primery btn clicked')
+			}
+		},
+		{
+			text: 'Cancel',
+			type: 'danger',
+			handler() {
+				console.log('Danger btn clicked')
+			}
+		}
+	]
 }
 
 const modal = $.modal(data)
-modal.open()
+// modal.open()
+
+/*
+	1. Динамически вывести список карточек из массива
+	2. Показывать цену в модалке (с кнопкой ok) при нажатии на кнопку price (это должна быть одна модалка)
+	3. Подтверждающая модалка для кнопки удалить с 2мя кнопками.
+*/
+
+const fruits = [
+	{id: 1, title: 'Apple', price: 20, img: 'https://e1.edimdoma.ru/data/ingredients/0000/2374/2374-ed4_wide.jpg?1487746348'},
+	{id: 2, title: 'Orange', price: 30, img: 'https://m.dom-eda.com/uploads/images/catalog/item/dfc9a3e974/3cbf3bd41c_500.jpg'},
+	{id: 3, title: 'Mango', price: 40, img: 'https://fnd.kiev.ua/uploads/product/000/4/thumbs/70_19-tayskoe-mango_2018-07-26_11-10-47.png'}
+]
--- plagins/modal.js
+++ plagins/modal.js
@@ -1,37 +1,62 @@
+Element.prototype.appendAfter = function(element) {
+	element.parentNode.insertBefore(this, element.nextSibling)
+}
+
+function noop(){}
+
+function _createModalFooter(buttons = []) {
+	if (buttons.length === 0) {
+		return document.createElement('div')
+	}
+	const wrap = document.createElement('div')
+	wrap.classList.add('modal-footer')
+
+	buttons.forEach(btn => {
+		const $btn = document.createElement('button')
+		$btn.textContent = btn.text
+		$btn.classList.add('btn')
+		$btn.classList.add(`btn-${btn.type || 'secondary'}`)
+		$btn.onclick = btn.handler || noop
+		wrap.appendChild($btn)
+	})
+	return wrap
+}
+
 function _createModal(options) {
+	const DEFAULT_WIDTH = '600px'
 	const modal = document.createElement('div')
 	modal.classList.add('vmodal')
 	modal.insertAdjacentHTML('afterbegin', `
-		<div class="modal-overlay">
-			<div class="modal-window" ${options.width ? `style="width:${options.width}"` : ''}>
+		<div class="modal-overlay" data-close>
+			<div class="modal-window" style="width:${options.width || DEFAULT_WIDTH}">
 				<div class="modal-header">
-					<span class="modal-title">${options.title}</span>
-					${options.closable ? '<span class="modal-close" onclick="modal.close()">&times;</span>' : ''}
+					<span class="modal-title">${options.title || "Modal window"}</span>
+					${options.closable ? '<span class="modal-close" data-close>&times;</span>' : ''}
 				</div>
-				<div class="modal-body">
-					<p>${options.content}</p>
-				</div>
-				<div class="modal-footer">
-					<button type="button">Ok</button>
-					<button type="button">Cancel</button>
+				<div class="modal-body" data-content>
+					${options.content || ""}
 				</div>
 			</div>
 		</div>
 	`)
-	document.body.appendChild(modal);
+	const footer = _createModalFooter(options.footerButtons)
+	footer.appendAfter(modal.querySelector('[data-content]'))
+	document.body.appendChild(modal)
 	return modal
 }
 
+
+
 /*
 options = {
-		title: string
-		closable: boolean
-		content: string
-		width: string ('400px') ? How implement style in not inline css and is it necessary?
-	destroy(): void
-	modal window should close
++	title: string
++	closable: boolean
++	content: string
++	width: string ('400px') ? How implement style in not inline css and is it necessary?
++	destroy(): void ??????
++	modal window should close
++	setContent(html: string): void | PUBLIC
 	-------------
-	setContent(html: string): void | PUBLIC
 	onClose(): void
 	onOpen(): void
 	beforeClose(): boolean | true - can close | false - can't close
@@ -44,9 +69,13 @@ $.modal = function(options) {
 	const ANIMATION_SPEED = 200
 	const $modal = _createModal(options)
 	let closing = false
+	let destroyed = false
 
-	return {
+	let modal = {
 		open() {
+			if (destroyed) {
+				return console.log('Modal is destroyed')
+			}
 			!closing && $modal.classList.add('open')
 		},
 		close() {
@@ -58,6 +87,22 @@ $.modal = function(options) {
 				closing = false
 			}, ANIMATION_SPEED)
 		},
-		destroy() {}
 	}
+
+	const handleClick = event => {
+		if (options.closable && (event.target.hasAttribute("data-close"))) modal.close()
+	}
+
+	$modal.addEventListener('click', handleClick)
+
+	return Object.assign(modal, {
+		destroy() {
+			$modal.parentNode.removeChild($modal)
+			$modal.removeEventListener('click', handleClick)
+			destroyed = true
+		},
+		setContent(html) {
+			$modal.querySelector('[data-content]').innerHTML = html
+		}
+	})
 }
